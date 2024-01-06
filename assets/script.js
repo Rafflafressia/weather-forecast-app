4379b8d8bd18744bc121c14852b5df10e67c4a98
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-01-05 20:31:23 -0500

    Updated styling of weather app

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,30 @@
-// API Call
+var searchBox = document.querySelector(".search-box");
+var searchBtn = document.querySelector(".search-button");
 
-// Submit Btn for City Search
+// api key variables
+const apiKey = "0b04baa169e04847ef24e7586e4a2756";
+const currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
-// 5 Day forecast For City
 
-// Current Weather For City
 
-// Current Time for City 
+
+
+
+
+
+
+
+async function checkCurrentWeather(city){
+    const response = await fetch(currentWeatherUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".")
+}
+
+
+
+searchBtn.addEventListener("click", function(){
+    checkCurrentWeather(searchBox.value);
+})
